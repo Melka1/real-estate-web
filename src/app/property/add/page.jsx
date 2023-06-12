d5e03fce296eedcaaa3b6126d59e3e494fc3d849
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2023-06-12 08:50:03 +0300

    add page preview pane started

--- src/app/property/add/page.jsx
+++ src/app/property/add/page.jsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import Imperative from '../../assets/aiImagePicker'
 import { NavBar } from '@/app/components'
 import InputSelect from '@/app/assets/inputSelect'
-import {AiOutlineLeft, AiOutlineCheck, AiOutlineMinus, AiOutlineCloudUpload, AiOutlinePlus} from 'react-icons/ai'
+import {AiOutlineLeft, AiOutlineCheck, AiOutlineMinus, AiOutlineCloudUpload, AiOutlinePlus, AiOutlineArrowLeft, AiOutlineHeart} from 'react-icons/ai'
 import {BsChevronRight} from 'react-icons/bs'
 import './add.css'
 import Checkbox from '@/app/assets/checkbox'
@@ -22,8 +22,29 @@ function page() {
     "Laundry every week, max 10Kg",
     "A security guard is provided"
   ])
+  const [addStatus, setAddStatus] = useState([true, true])
 
-  const [addStatus, setAddStatus] = useState([false, false])
+  const input1 = useRef("")
+  const input2 = useRef("")
+
+
+  function handleAddButton(id){
+    let addContent = [...addStatus]
+    console.log(addContent)
+    addContent[id] = !addContent[id]
+    console.log(addContent)
+    setAddStatus(addContent)
+  }
+
+  function handleAdd(id){
+    let content = id==0?input1.current.value:input2.current.value
+    console.log(content)
+    id==0?
+      setRules(prev=>[...prev, content])
+      :setAdditionalFacilities(prev=>[...prev, content])
+    handleAddButton(id)
+
+  }
  
   return (
     <div style={{minHeight:'100vh'}}>
@@ -88,7 +109,7 @@ function page() {
                   </div>
                 </div>
                 <hr/>
-                <div classname='unit--location'>
+                <div className='unit--location'>
                   <p className='unit--location--title section--title'>Unit Location</p>
                   <div className='unit--location--content'>
                     <label htmlFor="location--name">Unit name</label>
@@ -149,57 +170,87 @@ function page() {
                     <div className='additional--information--content--rules'>
                       <label htmlFor="rules">Rules</label>
                       {
-                        rules.map(rule=>(
-                          <Checkbox title={rule}/>
+                        rules.map((rule, index)=>(
+                          <Checkbox key={index} title={rule}/>
                         ))
                       }
-                      <button className='add--rule--and--facility'>
+                      {addStatus[0]?<button onClick={()=>handleAddButton(0)}  className='add--rule--and--facility'>
                         <AiOutlinePlus/>
                         <p>Add additional rules</p>
-                      </button>
+                      </button>:
                       <div className='input--container'>
-                        <input type='text' placeholder='Enter additional rule'/>
+                        <input ref={input1} type='text' placeholder='Enter additional rule'/>
                         <div className='input--controls'>
-                          <button onClick={()=>{}}>
+                          <button onClick={()=>handleAdd(0)}>
                             <p>Add</p>
                           </button>
-                          <button onClick={()=>{}}>
+                          <button onClick={()=>handleAddButton(0)}>
                             <p>Cancel</p>
                           </button>
                         </div>
+                      </div>}
+                      <div style={{marginTop:'1rem', borderRadius:'0.5rem', border:'1px dashed white', display:'flex', width:'100%', justifyContent:'center'}}>
+                        <Imperative style={{background:'transparent', border:'none', outline:'none'}}/>
                       </div>
                       
                     </div>
                     <div className='additional--information--content--facilities'>
                       <label htmlFor="additional--facilities">Additional facilities</label>
                       {
-                        additionalFacilities.map(facility =>(
-                          <Checkbox title={facility}/>
+                        additionalFacilities.map((facility, index) =>(
+                          <Checkbox key={index} title={facility}/>
                         ))
                       }
-                      <button className='add--rule--and--facility' onClick={()=>{}}>
+                     {addStatus[1]?<button onClick={()=>handleAddButton(1)} className='add--rule--and--facility'>
                         <AiOutlinePlus/>
                         <p>Add additional facilities</p>
-                      </button>
+                      </button>:
                       <div className='input--container'>
-                        <input type='text' placeholder='Enter additional facility'/>
+                        <input ref={input2} type='text' placeholder='Enter additional facility'/>
                         <div className='input--controls'>
-                          <button onClick={()=>{}}>
+                          <button onClick={()=>handleAdd(1)}>
                             <p>Add</p>
                           </button>
-                          <button onClick={()=>{}}>
+                          <button onClick={()=>handleAddButton(1)}>
                             <p>Cancel</p>
                           </button>
                         </div>
+                      </div>}
+                      <div style={{marginTop:'1rem', borderRadius:'0.5rem', border:'1px dashed white', display:'flex', width:'100%', justifyContent:'center'}}>
+                        <Imperative style={{background:'transparent', border:'none', outline:'none'}}/>
                       </div>
 
                     </div>
                   </div>
                 </div>
+                <hr/>
+                <div className='add--navigation'>
+                  <button className='back'>
+                    <AiOutlineArrowLeft fontSize={'1rem'}/>
+                    <p>Back</p>
+                  </button>
+                  <button className='continue'>
+                    <p>Continue</p>
+                  </button>
+                </div>
               </div>
             </div>
             <div className='partition preview--pane'>
               <h2>Quick Preview</h2>
+              <div className='preview--container'>
+                <div className='carousel--container'>
+                  //carousel
+                  //tags
+                </div>
+                <div className='preview--content--container'>
+                  <div className='price--and--like'>
+                    <p>$550</p>
+                    <div className='like--button'>
+                      <AiOutlineHeartLeft fontSize='1rem'/>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
 
